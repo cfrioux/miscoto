e8558a3d8f7579bddfc44ad943787fb78fe7680a
cfrioux <clemence.frioux@irisa.fr>
2019-03-18 08:44:59 +0100

    update version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 setup(
     name             = 'Miscoto',
-    version          = '1.1.4',
+    version          = '1.1.5',
     url              = 'https://github.com/cfrioux/miscoto',
     license          = 'GPLv3+',
     description      = 'Microbiome Screening and COmmunity selection using TOpology',
